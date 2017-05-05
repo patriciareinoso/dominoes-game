4d0d2326cc59159d930529f157483f6869258634
Patricia Reinoso <patricia.reinoso12@gmail.com>
2017-05-05 12:10:14 +0200

    Exceptions add on DominoInt class

--- src/generics/templates/GenericDomino.java
+++ src/generics/templates/GenericDomino.java
@@ -1,5 +1,6 @@
 package generics.templates;
 
+import softwareGame.DominoInt;
 import generics.abstractions.PairAbstraction;
 import generics.abstractions.PairSpecification;
 
@@ -19,14 +20,31 @@ public class GenericDomino<T> extends GenericPair<T> {
 	           return true;
 		 if (obj == null)
 	           return false;
-		 if (!(obj instanceof PairSpecification))
+		 //if (!(obj instanceof GenericDomino))
+		 if (!this.getClass().equals(obj.getClass()))
 	           return false;
 
-		 PairAbstraction other = (PairAbstraction) obj;
+		 GenericDomino other = (GenericDomino) obj;
 
 		 return ((other.getFirst().equals(getFirst()) && other.getSecond().equals(getSecond())) ||
 				 (other.getFirst().equals(getSecond()) && other.getSecond().equals(getFirst())));
 			
 	 }
+	
+	public static void main (String[] args){
+		GenericDomino<Integer> dom1 = new GenericDomino<Integer>(5,5);
+		GenericDomino<Character> dom2 = new GenericDomino<Character>('a','a');
+		GenericDomino<Integer> dom3 = new GenericDomino<Integer>(1,0);
+		GenericDomino<Integer> dom4 = new GenericDomino<Integer>(0,1);
+		GenericPair<Integer> dom6 = new GenericPair<Integer>(0,1);
+		System.out.println(dom1);
+		System.out.println(dom2.getFirst());
+		System.out.println(dom2.getSecond());
+		
+		System.out.println(dom2.equals(dom3));
+		System.out.println(dom2.equals(dom2));
+		System.out.println(dom3.equals(dom4));
+		System.out.println(dom4.equals(dom6));
+	}
 	
 }
--- src/softwareGame/DominoInt.java
+++ src/softwareGame/DominoInt.java
@@ -3,26 +3,52 @@ package softwareGame;
 /**
  * Represent each piece of the Domino Game. 
  * Extends the GenericDomino class in order to create dominoes chips
- * with integer number from 0 to 6.
+ * of a standard domino set.
  * Implements the InterfaceDomino interface in order to use the graphical interface.
+ * The value of each side of the Domino piece is between 0 and 6.
+ * Domino x:y is equal to Domino y:x
  * @author	Patricia REINOSO
- * @version 2.0
+ * @version 2.1.0
  * @since	2017-03-08
  */
 
 import graphicInterface.InterfaceDomino;
 import generics.templates.*;
+import tools.InvariantBrokenException;
 
 public class DominoInt extends GenericDomino implements InterfaceDomino{
 
 	/**
+	 * Minimun value of a side of a domino on the standard domino set 
+	 */
+	public static final int MIN = 0;
+	
+	/**
+	 * Maximum value of a side of a domino on the standard domino set 
+	 */
+	public static final int MAX = 6;
+	
+	public static final boolean INVARIANT = (MAX > MIN);
+	/**
 	 * Class constructor.
-	 * Uses the superclass'constructor.
+	 * Verifies if the arguments are valid
+	 * Constructs the DominoInt piece
+	 * Check if the invariant was not broken
 	 * @param leftValue  the first value of the piece.
 	 * @param rightValue the second value of the piece.
+	 * @throw IllegalArgumentException when leftValue or rightValue are out bounds.
+	 * @throw InvariantBrokenException if the domino state is not valid after execution.
 	 */
-	public DominoInt(Integer leftValue, Integer rightValue){
+	public DominoInt(Integer leftValue, Integer rightValue) throws IllegalArgumentException, InvariantBrokenException{
 		super(leftValue,rightValue);
+		if (leftValue < MIN || leftValue > MAX || rightValue < MIN || rightValue > MAX){
+			throw new IllegalArgumentException("Illegal arguments.");
+		}
+		setFirst(leftValue);
+		setSecond(rightValue);
+		if (!invariant()){
+			throw new InvariantBrokenException("Invariant broken.");
+		}
 	}
 
 	/**
@@ -45,12 +71,27 @@ public class DominoInt extends GenericDomino implements InterfaceDomino{
 		return val;
 	}
 
+	/**
+	 * Check that the left and right values are in allowed range 
+	 * @return True if the domino is in a valid state. False otherwise.
+	 */
+	public boolean invariant()  {
+		
+		if (getLeftValue() <= MAX && 
+			getLeftValue() >= MIN &&
+			getRightValue() <= MAX &&
+			getRightValue() >= MIN){
+			return true;
+		}
+		return false;
+	}
 
 	public static void main (String[] args){
 		DominoInt dom1 = new DominoInt(5,5);
 		DominoInt dom2 = new DominoInt(0,1);
 		DominoInt dom3 = new DominoInt(1,0);
 		DominoInt dom4 = new DominoInt(3,5);
+		GenericDomino<Integer> dom7 = new GenericDomino<Integer>(0,1);
 		
 		System.out.println(dom1);
 		System.out.println(dom2.getLeftValue());
@@ -59,6 +100,11 @@ public class DominoInt extends GenericDomino implements InterfaceDomino{
 		System.out.println(dom2.equals(dom3));
 		dom4.swap();
 		System.out.println(dom4);
+		DominoInt dom5 = new DominoInt(5,1);
+		DominoInt dom6 = new DominoInt(0,1);
+		System.out.println(dom5);
+		System.out.println(dom3.equals(dom7));
+		
 	}
 
 }
